f31b5523c820adb3f98dfd65d73bf0cf4b68675c
eduardoU-SM <eduardo.vasqueza@unmsm.edu.pe>
2026-04-02 21:19:06 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,18 @@
 module.exports = function(eleventyConfig) {
-  //REGISTRAMOS EL FILTRO "dateFilter"
+  // 1. FILTRO PARA FECHAS
   eleventyConfig.addFilter("dateFilter", (dateObj) => {
-    // Esto convierte la fecha a formato: 1 de abril de 2026
     return new Date(dateObj).toLocaleDateString('es-ES', {
       year: 'numeric',
       month: 'long',
       day: 'numeric'
     });
+  });
+
+ // 2. Filtro "limpiar" (ESTO ES LO QUE FALTA)
+  eleventyConfig.addFilter("limpiar", (content) => {
+    if (!content) return "";
+    // Elimina etiquetas HTML para que no rompan la tabla
+    return content.replace(/<\/?[^>]+(>|$)/g, "").trim();
   });
   // Copia la carpeta de assets (CSS, imágenes) al sitio final
   eleventyConfig.addPassthroughCopy("src/assets");
